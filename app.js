85274e8f52c950f7a91c9ec657f3ca634a7ac9bb
NoBey <786964300@qq.com>
2016-11-08 23:48:57 +0800

    update

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var app = express();
 var root = 'build'
 app.use(express.static(root))
 app.use(fallback('index.html', { root }))
-app.listen(2222);
+var server = app.listen(41892, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('TJCU site http://%s:%s', host, port);
+});
 
 // 用history-api-fallback 启动 spa
